a5246dbb16302c13d9fc43fabce7652deed1d6f7
Lucas da Silva Claros <74677953+lucasclaros@users.noreply.github.com>
2026-03-23 18:03:44 -0300

    scrap: aprovado recado de Teste Local

--- src/data/scraps.ts
+++ src/data/scraps.ts
@@ -12,4 +12,10 @@ export const scraps: Scrap[] = [
     message: "e aiii lucas blz mano?? bora bater uma bola sabado?? to montando um time aki :cool: chama os caras do TI tbm :thumbsup::thumbsup: abs :wink:",
     date: "2007-06-24",
   },
+  {
+    id: "1824bf71-e23f-4a96-b290-a439e335764b",
+    name: "Teste Local",
+    message: "testando o fluxo de scraps :smile:",
+    date: "2026-03-23",
+  },
 ];
