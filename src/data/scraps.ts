10e2b3c2a3fb31ff88698b65c96e3cb1b8d35415
Lucas da Silva Claros <74677953+lucasclaros@users.noreply.github.com>
2026-03-23 21:28:46 -0300

    scrap: removido recado 5dd8cf5c-b1b7-4515-885a-b80759925049

--- src/data/scraps.ts
+++ src/data/scraps.ts
@@ -12,10 +12,4 @@ export const scraps: Scrap[] = [
     message: "e aiii lucas blz mano?? bora bater uma bola sabado?? to montando um time aki :cool: chama os caras do TI tbm :thumbsup::thumbsup: abs :wink:",
     date: "2007-06-24",
   },
-  {
-    id: "5dd8cf5c-b1b7-4515-885a-b80759925049",
-    name: "testando gifs",
-    message: "testando gifs:sleepy:",
-    date: "2026-03-24",
-  },
 ];
