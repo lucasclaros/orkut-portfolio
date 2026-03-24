e825f417cae93f7bc4a3651e7dc87953bca4d06b
Lucas da Silva Claros <74677953+lucasclaros@users.noreply.github.com>
2026-03-24 13:34:01 -0300

    scrap: novo recado de ana

--- src/data/scraps.ts
+++ src/data/scraps.ts
@@ -12,4 +12,10 @@ export const scraps: Scrap[] = [
     message: "e aiii lucas blz mano?? bora bater uma bola sabado?? to montando um time aki :cool: chama os caras do TI tbm :thumbsup::thumbsup: abs :wink:",
     date: "2007-06-24",
   },
+  {
+    id: "27d9b3f0-992a-4b9b-aefa-d367e950e781",
+    name: "ana",
+    message: "amei :star::star::star:",
+    date: "2026-03-24",
+  },
 ];
